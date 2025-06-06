446f198551b31aa4d0a95ad5544474583ce53b33
Alice <alice@ritsu.dev>
2025-06-06 21:47:56 +1000

    initial

---
+++ electron.vite.config.ts
@@ -0,0 +1,31 @@
+import { resolve } from 'path'
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()],
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()],
+  },
+  renderer: {
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer/src'),
+      },
+    },
+    plugins: [
+      react({
+        babel: {
+          plugins: [['babel-plugin-react-compiler', {}]],
+        },
+      }),
+      tailwindcss(),
+    ],
+    define: {
+      __COMMMIT_HASH__: JSON.stringify(''),
+    },
+  },
+})
